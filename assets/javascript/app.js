e282ac7685edcdaa170c51a4ed3df9df11a2c0a2
Raul Gonzalez <rgonz166@gmail.com>
2019-07-28 23:53:24 -0700

    initial files, added placeholder and structure for website to add gif buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+/*
+
+Add functionality for getting values from inputs
+
+use jQuery to append buttons to top of page
+
+(try to have mobile uses search on top)
+
+have press of button remove the gifs and append new gifs to page ( or just prepend and keep the old ones in the bottom)
+
+
+
+*/
