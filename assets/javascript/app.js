1ada430b18ee5ad853b10d85d44a49c10de0af65
Raul Gonzalez <rgonz166@gmail.com>
2019-07-31 23:00:36 -0700

    created a clear all favorites buttons that is added once favorites have been added to local storage, added functionality that asks users if they are sure they want to clear favorites, if true then localStorage is emptied and favorite buttons deleted

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,10 @@
-/*
-
-Add functionality for getting values from inputs
-
-use jQuery to append buttons to top of page
-
-(try to have mobile uses search on top)
-
-have press of button remove the gifs and append new gifs to page ( or just prepend and keep the old ones in the bottom)
-
-add a event.preventDefault() because using submit button
+/* TODO
+add a remove from favorites
+    - populate checkbox only after a favorites has appeared
+add an empty all favorites with warning button, maybe an alert saying all favorites will be deleted
 
 */
+
 //Initialize variables
 var gifs = ['dog','cat'];
 var buttonColors = ['primary','secondary','success','danger','warning','info','dark'];
@@ -22,17 +16,17 @@ var gifContainer = $('.gif-container');
 var gifInput = $('#gif-input');
 var toggled = false;
 
-
+// Run when page loads
 $(window).on('load',function(){
-    // if localstorage is empty then run this
+    // if localstorage is not empty then run this
     if(!(localStorage.getItem('favId') === null)){
         favoriteGifs = JSON.parse(localStorage.getItem('favId'));
         console.log('parsed',favoriteGifs);
     }
-    
     // Show favorite button if favorite array contains anything
     if(favoriteGifs.length > 0){
         addFavButton();
+        addClearAllFavButton();
     }
     loadButtons();
 });
@@ -45,26 +39,11 @@ $('#add-gif').on('click',function(e){
     if(gifInput.val().trim() === ''){
         return;
     }
-    else{gifs.push(gifInput.val().trim());}
+    else{
+        gifs.push(gifInput.val().trim());
+    }
     gifInput.val('');
     loadButtons();
-});
-
-$(document).on('click','#favorites',function(e){
-    e.preventDefault();
-    favoriteGifs = JSON.parse(localStorage.getItem('favId'));
-    // var favoriteIDs = favoriteGifs.join(',');
-    console.log('fav id string', favoriteGifs);
-    
-    var favoriteQ = 'http://api.giphy.com/v1/gifs?api_key=dc6zaTOxFJmzC&ids='+favoriteGifs+'';
-    displayGifs(favoriteQ);
-});
-
-$(document).on('click','.gif-button',function(){
-    var gifQ = 'q="'+($(this).attr('data-name'))+'"';
-    var key = 'iflq4o1A7c7VD2FODJs0Hw2Q3dzfBKwy';
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?' + gifQ + '&api_key='+key+'&limit=10';
-    displayGifs(queryURL);
 });
 
 function loadButtons(){
@@ -86,6 +65,16 @@ function loadButtons(){
     }
 }
 
+// When a gif button is clicked run this
+$(document).on('click','.gif-button',function(){
+    // query to search for gifs, get name of button clicked with the name of the query
+    var gifQ = 'q="'+($(this).attr('data-name'))+'"';
+    // key used for api
+    var key = 'iflq4o1A7c7VD2FODJs0Hw2Q3dzfBKwy';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?' + gifQ + '&api_key='+key+'&limit=10';
+    displayGifs(queryURL);
+});
+
 function displayGifs(query){
     $.ajax({
         url:query,
@@ -95,7 +84,6 @@ function displayGifs(query){
         var results = res.data;
         // Looping through each result item
         for (var i = 0; i < results.length; i++) {
-
             // Creating and storing a div tag
             var gifDiv = $("<div>");
             gifDiv.addClass('gif-div');
@@ -124,33 +112,34 @@ function displayGifs(query){
     });
 }
 
+// Toggle flag when add to favorite toggle is pressed
 function toggleFavoriteButton(){
+    // if toggle is true set it to false
     if(toggled){
         toggled = false;
+    // if toggle is false set it to true
     }else{
         toggled = true;
     }
 }
 
-function addFavButton(){
-    console.log('fav array len',favoriteGifs.length);
-    
-        var favButton = $('<button>');
-        favButton.attr('id','favorites');
-        favButton.attr('class', 'btn btn-info');
-        favButton.text('Favorites');
-        $('#gif-form').append(favButton);
-    
-}
-
+// when a gif image is clicked run this
 $(document).on('click','.gif',function(){
+    // store true or false of toggle
     var favoriteToggle = $('.fav-toggle');
+    // if toggle is true, then run this
     if(toggled){
+        // if toggled then do not animate
+        // show favorite button and delete all favorite button on page
         if(favoriteGifs.length == 0){
             addFavButton();
+            addClearAllFavButton();
         }
+        // push the gif id to local favorite gifs array
         favoriteGifs.push($(this).attr('data-id'));
+        // then store favorite gifs array into localStorage to save even after reloading
         localStorage.setItem("favId",JSON.stringify(favoriteGifs));
+    // If not toggled run this
     }else{
         var state = $(this).attr("data-state");
         // If the clicked image's state is still, update its src attribute to what its data-animate value is.
@@ -165,6 +154,44 @@ $(document).on('click','.gif',function(){
         }
     }
 });
+
+// adds Favorite button to page
+function addFavButton(){
+    var favButton = $('<button>');
+    favButton.attr('id','favorites');
+    favButton.attr('class', 'btn btn-info');
+    favButton.text('Favorites');
+    $('#gif-form').append(favButton);
+}
+
+$(document).on('click','#favorites',function(e){
+    e.preventDefault();
+    favoriteGifs = JSON.parse(localStorage.getItem('favId'));
+    // var favoriteIDs = favoriteGifs.join(',');
+    console.log('fav id string', favoriteGifs);
+    
+    var favoriteQ = 'http://api.giphy.com/v1/gifs?api_key=dc6zaTOxFJmzC&ids='+favoriteGifs+'';
+    displayGifs(favoriteQ);
+});
+
+// add clears all favorites from local Storage
+function addClearAllFavButton(){
+var clearAllFavButton = $('<button>');
+clearAllFavButton.attr('id','clearAllFav');
+clearAllFavButton.attr('class', 'btn btn-danger');
+clearAllFavButton.text('Clear All Favorites');
+$('#gif-form').append(clearAllFavButton);
+}
+
+$(document).on('click','#clearAllFav',function(e){
+    e.preventDefault();
+    if(confirm('Are you sure you want to clear all your favorites?')){
+        localStorage.clear();
+        favoriteGifs = [];
+        $('#favorites').remove();
+        $('#clearAllFav').remove();
+    }
+})
 
 
 // Function to reset page of buttons and gifs
